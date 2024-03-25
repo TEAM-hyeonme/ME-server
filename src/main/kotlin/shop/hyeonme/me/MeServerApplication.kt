a96663372f3fa86aea20933a57ea6f9ffffced5c
ani2689 <ojno7777@gmail.com>
2024-03-25 11:45:00 +0900

    create :: 프로젝트 생성

---
+++ src/main/kotlin/shop/hyeonme/me/MeServerApplication.kt
@@ -0,0 +1,11 @@
+package shop.hyeonme.me
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<MeServerApplication>(*args)
+}
