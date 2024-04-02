a9eb89354388d0ebe9706e4b74357da3401e27b8
ani2689 <ojno7777@gmail.com>
2024-04-02 20:00:56 +0900

    update :: 메인클래스 정의

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
     kotlin("jvm") version "1.7.10"
     id("org.springframework.boot") version "2.7.5"
@@ -52,6 +54,6 @@ allprojects {
 
 }
 
-tasks.bootJar {
+tasks.withType<BootJar> {
     mainClass.set("shop.hyeonme.me.MeServerApplication")
 }
