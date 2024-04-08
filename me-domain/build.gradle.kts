0f8bf165deded84bfa7627ee8d132092be6ceaa9
ani2689 <ojno7777@gmail.com>
2024-04-08 14:15:39 +0900

    fix :: CD yml 순서 변경

--- me-domain/build.gradle.kts
+++ me-domain/build.gradle.kts
@@ -12,6 +12,9 @@ jar.enabled = true
 bootJar.enabled = false
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis:2.7.5")
+    runtimeOnly ("com.mysql:mysql-connector-j")
 
     implementation("com.querydsl:querydsl-jpa:5.0.0")
     kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
