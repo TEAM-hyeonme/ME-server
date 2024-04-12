9e95bf83cebf550ff48d3414c1f3534c86fecb37
ani2689 <ojno7777@gmail.com>
2024-04-12 11:21:13 +0900

    update :: column 설정 추가

--- me-domain/src/main/kotlin/shop/hyeonme/me/common/base/BaseLongEntity.kt
+++ me-domain/src/main/kotlin/shop/hyeonme/me/common/base/BaseLongEntity.kt
@@ -1,16 +1,14 @@
 package shop.hyeonme.me.common.base
 
 import org.springframework.data.domain.Persistable
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
-import javax.persistence.MappedSuperclass
+import javax.persistence.*
 
 @MappedSuperclass
 abstract class BaseLongEntity(
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    open var id: Long = 0L
+    @Column(name = "id", nullable = false, unique = true)
+    open val id: Long = 0L
 ) : BaseTimeEntity(), Persistable<Long> {
     override fun getId() = id
 
