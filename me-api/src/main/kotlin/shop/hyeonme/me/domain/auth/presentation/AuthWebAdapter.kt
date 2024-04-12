f9198605aa6f13367f4f38da7a7909786f66a14a
ani2689 <ojno7777@gmail.com>
2024-04-12 14:03:01 +0900

    create :: get apple signin url mapping

--- me-api/src/main/kotlin/shop/hyeonme/me/domain/auth/presentation/AuthWebAdapter.kt
+++ me-api/src/main/kotlin/shop/hyeonme/me/domain/auth/presentation/AuthWebAdapter.kt
@@ -2,6 +2,7 @@ package shop.hyeonme.me.domain.auth.presentation
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import shop.hyeonme.me.common.annotation.WebAdapter
@@ -9,13 +10,21 @@ import shop.hyeonme.me.domain.auth.mapper.toRequest
 import shop.hyeonme.me.domain.auth.mapper.toResponse
 import shop.hyeonme.me.domain.auth.presentation.web.req.AppleSignInWebRequest
 import shop.hyeonme.me.domain.auth.presentation.web.res.AppleSignInWebResponse
+import shop.hyeonme.me.domain.auth.presentation.web.res.GetAppleSignInUrlWebResponse
 import shop.hyeonme.me.domain.auth.usecase.AppleSignInUseCase
+import shop.hyeonme.me.domain.auth.usecase.GetAppleSignInUrlUseCase
 import javax.validation.Valid
 
 @WebAdapter("/auth")
 class AuthWebAdapter(
+    private val getAppleSignInUrlUseCase: GetAppleSignInUrlUseCase,
     private val appleSignInUseCase: AppleSignInUseCase
 ) {
+    @GetMapping("/apple")
+    fun getAppleSignInUrl(): ResponseEntity<GetAppleSignInUrlWebResponse> =
+        getAppleSignInUrlUseCase.execute()
+            .let { ResponseEntity.status(HttpStatus.OK).body(it.toResponse()) }
+
     @PostMapping("/apple")
     fun appleSignIn(@RequestBody @Valid webRequest: AppleSignInWebRequest): ResponseEntity<AppleSignInWebResponse> =
         appleSignInUseCase.execute(webRequest.toRequest())
