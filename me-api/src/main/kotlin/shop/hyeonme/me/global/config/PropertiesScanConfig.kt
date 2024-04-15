8d635d430f18d72ca73afcc1cc8af4895caeb567
ani2689 <ojno7777@gmail.com>
2024-04-16 01:24:53 +0900

    update :: propertiesScan class 이동

---
+++ me-api/src/main/kotlin/shop/hyeonme/me/global/config/PropertiesScanConfig.kt
@@ -0,0 +1,12 @@
+package shop.hyeonme.me.global.config
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
+import shop.hyeonme.me.thirdparty.apple.properties.AppleProperties
+
+@Configuration
+@ConfigurationPropertiesScan(
+    basePackageClasses = [
+        AppleProperties::class,
+    ])
+class PropertiesScanConfig
