2031540c6ca11643563e859c0594ef66109c3a99
ani2689 <ojno7777@gmail.com>
2024-04-16 01:25:42 +0900

    update :: 서비스들 어노테이션 추가

--- me-api/src/main/kotlin/shop/hyeonme/me/global/config/ComponentScanConfig.kt
+++ me-api/src/main/kotlin/shop/hyeonme/me/global/config/ComponentScanConfig.kt
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.ComponentScan.Filter
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.FilterType
+import shop.hyeonme.me.common.annotation.CommandService
+import shop.hyeonme.me.common.annotation.QueryService
 import shop.hyeonme.me.common.annotation.Service
 import shop.hyeonme.me.common.annotation.UseCase
 
@@ -15,9 +17,11 @@ import shop.hyeonme.me.common.annotation.UseCase
             type = FilterType.ANNOTATION,
             classes = [
                 UseCase::class,
-                Service::class
+                Service::class,
+                QueryService::class,
+                CommandService::class
             ]
         )
     ]
 )
-class ComponentScanConfiguration
+class ComponentScanConfig
