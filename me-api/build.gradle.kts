2c879e64f34ae3339199a4b1cf4cab174d0d22ff
ani2689 <ojno7777@gmail.com>
2024-04-16 14:23:02 +0900

    create :: jwt 설정

--- me-api/build.gradle.kts
+++ me-api/build.gradle.kts
@@ -6,8 +6,13 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     kapt("org.springframework.boot:spring-boot-configuration-processor")
 
-    implementation(project(":me-domain"))
-    implementation(kotlin("stdlib-jdk11"))
+    runtimeOnly ("com.mysql:mysql-connector-j")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 kapt {
--- me-api/src/main/kotlin/shop/hyeonme/me/global/config/PropertiesScanConfig.kt
+++ me-api/src/main/kotlin/shop/hyeonme/me/global/config/PropertiesScanConfig.kt
@@ -2,11 +2,13 @@ package shop.hyeonme.me.global.config
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Configuration
+import shop.hyeonme.me.global.jwt.properties.JwtProperties
 import shop.hyeonme.me.thirdparty.apple.properties.AppleProperties
 
 @Configuration
 @ConfigurationPropertiesScan(
     basePackageClasses = [
         AppleProperties::class,
+        JwtProperties::class
     ])
 class PropertiesScanConfig
