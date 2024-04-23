28a9cba5d1405b5a3efc759f730245597373d88b
ani2689 <ojno7777@gmail.com>
2024-04-23 14:12:06 +0900

    update :: 예외 메세지 변경

--- me-infra/src/main/kotlin/shop/hyeonme/global/error/GlobalErrorCode.kt
+++ me-infra/src/main/kotlin/shop/hyeonme/global/error/GlobalErrorCode.kt
@@ -6,8 +6,8 @@ enum class GlobalErrorCode(
 ) {
     INTERNAL_SERVER_ERROR("서버 내부 에러", 500),
     BAD_REQUEST("잘못된 요청입니다.", 400),
-    INVALID_ROLE("검증되지 않은 권한입니다.", 401),
-    FORBIDDEN("FORBIDDEN", 403),
+    INVALID_ROLE("권한을 증명할 수 없는 사용자입니다.", 401),
+    FORBIDDEN("접근할 권한이 없습니다.", 403),
     EXPIRED_TOKEN("토큰이 만료되었습니다.", 401),
     INVALID_TOKEN("유효하지 않은 토큰입니다.", 401),
     LOCK_INTERRUPTED("락 획득에 실패했습니다", 500)
