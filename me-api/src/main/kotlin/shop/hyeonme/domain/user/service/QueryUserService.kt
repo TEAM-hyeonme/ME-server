fe4b84accb0897c0fd28a865188dd87b03740d30
ani2689 <ojno7777@gmail.com>
2024-04-23 17:04:55 +0900

    update :: findById 추가

--- me-api/src/main/kotlin/shop/hyeonme/domain/user/service/QueryUserService.kt
+++ me-api/src/main/kotlin/shop/hyeonme/domain/user/service/QueryUserService.kt
@@ -1,8 +1,10 @@
 package shop.hyeonme.domain.user.service
 
 import shop.hyeonme.domain.user.model.User
+import java.util.UUID
 
 interface QueryUserService {
     fun existByEmail(email: String): Boolean
     fun findByEmail(email: String): User?
+    fun findById(id: UUID): User
 }
--- me-api/src/main/kotlin/shop/hyeonme/domain/user/spi/QueryUserPort.kt
+++ me-api/src/main/kotlin/shop/hyeonme/domain/user/spi/QueryUserPort.kt
@@ -1,8 +1,10 @@
 package shop.hyeonme.domain.user.spi
 
 import shop.hyeonme.domain.user.model.User
+import java.util.UUID
 
 interface QueryUserPort {
     fun existByEmail(email: String): Boolean
     fun findByEmail(email: String): User?
+    fun findById(id: UUID): User?
 }
