9055f37a84d7d94028bdac3df06c2fb59955b571
ani2689 <ojno7777@gmail.com>
2024-05-01 13:55:31 +0900

    add :: created at

--- me-api/src/main/kotlin/shop/hyeonme/domain/exercise/model/Exercise.kt
+++ me-api/src/main/kotlin/shop/hyeonme/domain/exercise/model/Exercise.kt
@@ -2,10 +2,12 @@ package shop.hyeonme.domain.exercise.model
 
 import shop.hyeonme.domain.exercise.model.enums.ExerciseType
 import shop.hyeonme.domain.user.model.User
+import java.time.LocalDateTime
 
 data class Exercise(
     val id: Long = 0L,
     val calorie: Int,
     val type: ExerciseType,
-    val user: User
+    val user: User,
+    val createdDate: LocalDateTime
 )
--- me-api/src/main/kotlin/shop/hyeonme/domain/order/model/Order.kt
+++ me-api/src/main/kotlin/shop/hyeonme/domain/order/model/Order.kt
@@ -1,10 +1,12 @@
 package shop.hyeonme.domain.order.model
 
 import shop.hyeonme.domain.coupon.model.Coupon
+import java.time.LocalDateTime
 import java.util.UUID
 
 data class Order(
     val id: Long = 0L,
     val userId: UUID,
-    val coupon: Coupon
+    val coupon: Coupon,
+    val createdDate: LocalDateTime
 )
