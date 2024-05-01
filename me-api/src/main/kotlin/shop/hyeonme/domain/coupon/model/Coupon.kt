1ac649579da909252f4130fc0ada9d13d68e2350
ani2689 <ojno7777@gmail.com>
2024-05-01 19:53:21 +0900

    update :: import문 정리

--- me-api/src/main/kotlin/shop/hyeonme/domain/coupon/model/Coupon.kt
+++ me-api/src/main/kotlin/shop/hyeonme/domain/coupon/model/Coupon.kt
@@ -1,7 +1,6 @@
 package shop.hyeonme.domain.coupon.model
 
 import shop.hyeonme.domain.inventory.model.Inventory
-import shop.hyeonme.domain.user.model.User
 import java.time.LocalDateTime
 import java.util.UUID
 
--- me-api/src/main/kotlin/shop/hyeonme/domain/order/model/Order.kt
+++ me-api/src/main/kotlin/shop/hyeonme/domain/order/model/Order.kt
@@ -3,7 +3,6 @@ package shop.hyeonme.domain.order.model
 import shop.hyeonme.domain.coupon.model.Coupon
 import shop.hyeonme.domain.point.model.UsedPoint
 import java.time.LocalDateTime
-import java.util.UUID
 
 data class Order(
     val id: Long = 0L,
