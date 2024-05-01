78b9b8fb1321852a1fcf754c7881cc4593fd126c
ani2689 <ojno7777@gmail.com>
2024-05-01 19:52:23 +0900

    create :: domain mapper

--- me-api/src/main/kotlin/shop/hyeonme/domain/order/model/Order.kt
+++ me-api/src/main/kotlin/shop/hyeonme/domain/order/model/Order.kt
@@ -1,12 +1,13 @@
 package shop.hyeonme.domain.order.model
 
 import shop.hyeonme.domain.coupon.model.Coupon
+import shop.hyeonme.domain.point.model.UsedPoint
 import java.time.LocalDateTime
 import java.util.UUID
 
 data class Order(
     val id: Long = 0L,
-    val userId: UUID,
     val coupon: Coupon,
+    val usedPoint: UsedPoint,
     val createdAt: LocalDateTime
 )
--- me-api/src/main/kotlin/shop/hyeonme/domain/point/model/Point.kt
+++ me-api/src/main/kotlin/shop/hyeonme/domain/point/model/Point.kt
@@ -4,6 +4,5 @@ import java.util.*
 
 data class Point(
     val id: Long = 0L,
-    val amount: Int,
     val userId: UUID
 )
