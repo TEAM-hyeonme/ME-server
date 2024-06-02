ab55731bcaf09851275a1cdcaf0b90f96090c1ed
ani2689 <ojno7777@gmail.com>
2024-06-02 23:00:54 +0900

    Revert "create :: point service"
    This reverts commit 2ec9b74d24cfe4b2ec1db57cee3f767da06a4f0a.

--- me-api/src/main/kotlin/shop/hyeonme/domain/point/service/CommandPointService.kt
+++
@@ -1,7 +0,0 @@
-package shop.hyeonme.domain.point.service
-
-import shop.hyeonme.domain.point.model.TotalPoint
-
-interface CommandPointService {
-    fun saveTotalPoint(totalPoint: TotalPoint): TotalPoint
-}
