82cb928fa291b3ef64391a49928c2d783c7528de
ani2689 <ojno7777@gmail.com>
2024-06-04 00:07:50 +0900

    delete :: total point 삭제

--- me-api/src/main/kotlin/shop/hyeonme/domain/exercise/usecase/UpdateExercisesUseCase.kt
+++ me-api/src/main/kotlin/shop/hyeonme/domain/exercise/usecase/UpdateExercisesUseCase.kt
@@ -4,21 +4,25 @@ import shop.hyeonme.common.annotation.UseCase
 import shop.hyeonme.domain.exercise.model.Exercise
 import shop.hyeonme.domain.exercise.service.ExerciseService
 import shop.hyeonme.domain.exercise.usecase.data.req.UpdateExercisesRequestData
+import shop.hyeonme.domain.point.model.SavedPoint
+import shop.hyeonme.domain.point.service.PointService
 import shop.hyeonme.domain.user.service.UserService
 import kotlin.math.max
+import kotlin.math.min
 
 @UseCase
 class UpdateExercisesUseCase(
     private val userService: UserService,
-    private val exerciseService: ExerciseService
+    private val exerciseService: ExerciseService,
+    private val pointService: PointService
 ) {
     fun execute(request: UpdateExercisesRequestData) {
         val userId = userService.findCurrentUserId()
 
-        val currentExercises = exerciseService.findExerciseByCurrentDate(userId)
+        val currentDateExercises = exerciseService.findExerciseByCurrentDate(userId)
 
         val exercises = request.exercises.map { exercise ->
-            currentExercises.find { it.type == exercise.exerciseType }
+            currentDateExercises.find { it.type == exercise.exerciseType }
                 ?.run {
                     copy(
                         calorie = max(calorie, exercise.calorie)
@@ -32,5 +36,19 @@ class UpdateExercisesUseCase(
         }
 
         exerciseService.saveExercises(exercises)
+
+        val currentDateSavedPoint = pointService.findSavedPointByCurrentDate(userId)
+            ?: SavedPoint(
+                amount = 0,
+                userId = userId,
+            )
+
+        val totalCalorie = exercises.sumOf { it.calorie }
+
+        val savedPoint = currentDateSavedPoint.copy(
+            amount = min(100, totalCalorie/100)
+        )
+
+        pointService.savePoint(savedPoint)
     }
 }
--- me-domain/src/main/kotlin/shop/hyeonme/domain/point/repository/PointRepository.kt
+++
@@ -1,6 +0,0 @@
-package shop.hyeonme.domain.point.repository
-
-import org.springframework.data.repository.CrudRepository
-import shop.hyeonme.domain.point.entity.TotalPointEntity
-
-interface PointRepository : CrudRepository<TotalPointEntity, Long>
