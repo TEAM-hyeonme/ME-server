70061d606942c6177bc4b29415877252c29062a3
ani2689 <ojno7777@gmail.com>
2024-06-04 23:18:04 +0900

    add :: query exercises web api

--- me-api/src/main/kotlin/shop/hyeonme/domain/exercise/presentation/ExerciseWebAdapter.kt
+++ me-api/src/main/kotlin/shop/hyeonme/domain/exercise/presentation/ExerciseWebAdapter.kt
@@ -9,7 +9,9 @@ import shop.hyeonme.common.annotation.WebAdapter
 import shop.hyeonme.domain.exercise.mapper.toRequest
 import shop.hyeonme.domain.exercise.mapper.toResponse
 import shop.hyeonme.domain.exercise.presentation.web.req.UpdateExercisesWebRequest
+import shop.hyeonme.domain.exercise.presentation.web.res.QueryExercisesWebResponse
 import shop.hyeonme.domain.exercise.presentation.web.res.QueryTopExercisesWebResponse
+import shop.hyeonme.domain.exercise.usecase.QueryExercisesUseCase
 import shop.hyeonme.domain.exercise.usecase.QueryTopExercisesUseCase
 import shop.hyeonme.domain.exercise.usecase.UpdateExercisesUseCase
 import javax.validation.Valid
@@ -17,12 +19,19 @@ import javax.validation.Valid
 @WebAdapter("/exercise")
 class ExerciseWebAdapter(
     private val updateExercisesUseCase: UpdateExercisesUseCase,
+    private val queryExercisesUseCase: QueryExercisesUseCase,
     private val queryTopExercisesUseCase: QueryTopExercisesUseCase
 ) {
     @PutMapping
     fun updateExercises(@Valid @RequestBody webRequest: UpdateExercisesWebRequest): ResponseEntity<Unit> =
         updateExercisesUseCase.execute(webRequest.toRequest())
             .let { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
+
+    @GetMapping
+    fun queryExercises(): ResponseEntity<QueryExercisesWebResponse> =
+        queryExercisesUseCase.execute()
+            .let { ResponseEntity.status(HttpStatus.OK).body(it.toResponse()) }
+
 
     @GetMapping("/top")
     fun queryTopExercises(): ResponseEntity<QueryTopExercisesWebResponse> =
---
+++ me-api/src/main/kotlin/shop/hyeonme/domain/exercise/usecase/QueryExercisesUseCase.kt
@@ -0,0 +1,33 @@
+package shop.hyeonme.domain.exercise.usecase
+
+import shop.hyeonme.common.annotation.ReadOnlyUseCase
+import shop.hyeonme.domain.exercise.service.ExerciseService
+import shop.hyeonme.domain.exercise.usecase.data.res.QueryExerciseResponseData
+import shop.hyeonme.domain.exercise.usecase.data.res.QueryExercisesResponseData
+import shop.hyeonme.domain.point.service.PointService
+import shop.hyeonme.domain.user.service.UserService
+
+@ReadOnlyUseCase
+class QueryExercisesUseCase(
+    private val exerciseService: ExerciseService,
+    private val userService: UserService,
+    private val pointService: PointService
+) {
+    fun execute(): QueryExercisesResponseData {
+        val userId = userService.findCurrentUserId()
+        val point = pointService.countSavedPointByCurrentDate(userId)
+
+        val exercises = exerciseService.findExercisesByCurrentDate(userId)
+            .map {
+                QueryExerciseResponseData(
+                    exerciseType = it.type,
+                    calorie = it.calorie
+                )
+            }
+
+        return QueryExercisesResponseData(
+            point = point,
+            exercises = exercises
+        )
+    }
+}
