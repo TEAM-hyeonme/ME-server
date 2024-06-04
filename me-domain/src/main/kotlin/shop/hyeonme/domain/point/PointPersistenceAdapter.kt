5c90537b42bad234531c6480fd5d7607acf35b35
ani2689 <ojno7777@gmail.com>
2024-06-04 23:34:35 +0900

    add :: point port 구현

--- me-domain/src/main/kotlin/shop/hyeonme/domain/point/PointPersistenceAdapter.kt
+++ me-domain/src/main/kotlin/shop/hyeonme/domain/point/PointPersistenceAdapter.kt
@@ -1,6 +1,9 @@
 package shop.hyeonme.domain.point
 
+import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Component
+import shop.hyeonme.domain.exercise.entity.QExerciseEntity.exerciseEntity
+import shop.hyeonme.domain.point.entity.QSavedPointEntity.savedPointEntity
 import shop.hyeonme.domain.point.mapper.toEntity
 import shop.hyeonme.domain.point.mapper.toModel
 import shop.hyeonme.domain.point.model.SavedPoint
@@ -12,6 +15,7 @@ import java.util.*
 
 @Component
 class PointPersistenceAdapter(
+    private val queryFactory: JPAQueryFactory,
     private val savedPointRepository: SavedPointRepository,
     private val usedPointRepository: UsedPointRepository
 ) : PointPort {
@@ -20,4 +24,17 @@ class PointPersistenceAdapter(
 
     override fun findSavedPointByDate(userId: UUID, date: LocalDate): SavedPoint? =
         savedPointRepository.findByUserIdAndCreatedAt(userId, date)?.toModel()
+
+    override fun countSavedPointByDate(userId: UUID, date: LocalDate): Int =
+        queryFactory
+            .select(
+                savedPointEntity.amount
+            )
+            .from(savedPointEntity)
+            .where(
+                exerciseEntity.userId.eq(userId),
+                exerciseEntity.createdAt.eq(date)
+            )
+            .fetchOne() ?: 0
+
 }
