e005be28ae3f916f378d11dbcd971d73d9140994
노현주 <dev.hyeonju@gmail.com>
2024-07-24 19:52:55 +0900

    update :: 반환 값 추가

--- me-api/src/main/kotlin/shop/hyeonme/domain/gifticon/presentation/GifticonWebAdapter.kt
+++ me-api/src/main/kotlin/shop/hyeonme/domain/gifticon/presentation/GifticonWebAdapter.kt
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import shop.hyeonme.common.annotation.WebAdapter
 import shop.hyeonme.domain.gifticon.mapper.toRequest
+import shop.hyeonme.domain.gifticon.mapper.toResponse
 import shop.hyeonme.domain.gifticon.presentation.web.req.CreateGifticonWebRequest
+import shop.hyeonme.domain.gifticon.presentation.web.res.CreateGifticonWebResponse
 import shop.hyeonme.domain.gifticon.usecase.CreateGifticonUseCase
 import javax.validation.Valid
 
@@ -15,7 +17,7 @@ class GifticonWebAdapter(
     private val createGifticonUseCase: CreateGifticonUseCase,
 ) {
     @PostMapping("/admin")
-    fun createGifticon(@RequestBody @Valid request: CreateGifticonWebRequest): ResponseEntity<Unit> =
+    fun createGifticon(@RequestBody @Valid request: CreateGifticonWebRequest): ResponseEntity<CreateGifticonWebResponse> =
         createGifticonUseCase.execute(request.toRequest())
-            .let { ResponseEntity.status(HttpStatus.CREATED).build() }
+            .let { ResponseEntity.status(HttpStatus.CREATED).body(it.toResponse()) }
 }
