84eeaa0f6b5e68f9d301253537e1db5831fc527b
노현주 <dev.hyeonju@gmail.com>
2024-07-24 19:53:50 +0900

    update :: create usecase 작성

--- me-api/src/main/kotlin/shop/hyeonme/domain/gifticon/usecase/CreateGifticonUseCase.kt
+++ me-api/src/main/kotlin/shop/hyeonme/domain/gifticon/usecase/CreateGifticonUseCase.kt
@@ -1,11 +1,28 @@
 package shop.hyeonme.domain.gifticon.usecase
 
 import shop.hyeonme.common.annotation.UseCase
+import shop.hyeonme.domain.gifticon.model.Gifticon
+import shop.hyeonme.domain.gifticon.service.GifticonService
 import shop.hyeonme.domain.gifticon.usecase.data.req.CreateGifticonRequestData
+import shop.hyeonme.domain.gifticon.usecase.data.res.CreateGifticonResponseData
 
 @UseCase
-class CreateGifticonUseCase {
-    fun execute(request: CreateGifticonRequestData) {
+class CreateGifticonUseCase(
+    private val gifticonService: GifticonService
+) {
+    fun execute(request: CreateGifticonRequestData): CreateGifticonResponseData {
+        val gifticon = request.run {
+            Gifticon(
+                name = name,
+                brand = brand,
+                profileUrl = profileUrl,
+                notice = notice,
+                point = point
+            )
+        }.run(gifticonService::saveGifticon)
 
+        return CreateGifticonResponseData(
+            id = gifticon.id
+        )
     }
 }
