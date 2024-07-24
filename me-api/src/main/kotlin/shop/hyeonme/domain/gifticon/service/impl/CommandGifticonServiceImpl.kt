da1fe768e9265416ff0e92c282045c5a6766486d
노현주 <dev.hyeonju@gmail.com>
2024-07-25 00:28:36 +0900

    add :: query gifticon service

--- me-api/src/main/kotlin/shop/hyeonme/domain/gifticon/service/impl/CommandGifticonServiceImpl.kt
+++ me-api/src/main/kotlin/shop/hyeonme/domain/gifticon/service/impl/CommandGifticonServiceImpl.kt
@@ -1,11 +1,11 @@
 package shop.hyeonme.domain.gifticon.service.impl
 
-import shop.hyeonme.common.annotation.QueryService
+import shop.hyeonme.common.annotation.CommandService
 import shop.hyeonme.domain.gifticon.model.Gifticon
 import shop.hyeonme.domain.gifticon.service.CommandGifticonService
 import shop.hyeonme.domain.gifticon.spi.GifticonPort
 
-@QueryService
+@CommandService
 class CommandGifticonServiceImpl(
     private val gifticonPort: GifticonPort
 ) : CommandGifticonService {
