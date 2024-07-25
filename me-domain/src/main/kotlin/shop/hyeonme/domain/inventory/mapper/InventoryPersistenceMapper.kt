5acf4d5f1f725faa234618d510fbfa9f367d90de
노현주 <dev.hyeonju@gmail.com>
2024-07-25 17:13:18 +0900

    add :: port 구현

--- me-domain/src/main/kotlin/shop/hyeonme/domain/inventory/mapper/InventoryPersistenceMapper.kt
+++ me-domain/src/main/kotlin/shop/hyeonme/domain/inventory/mapper/InventoryPersistenceMapper.kt
@@ -5,13 +5,15 @@ import shop.hyeonme.domain.gifticon.mapper.toModel
 import shop.hyeonme.domain.inventory.entity.InventoryEntity
 import shop.hyeonme.domain.inventory.model.Inventory
 
-fun Inventory.toDomain() = InventoryEntity(
+fun Inventory.toEntity() = InventoryEntity(
     id = id,
     createdAt = createdAt,
     expiredAt = expiredAt,
     barcodeUrl = barcodeUrl,
     gifticon = gifticon.toEntity()
 )
+
+fun List<Inventory>.toEntities() = map(Inventory::toEntity)
 
 fun InventoryEntity.toModel() = Inventory(
     id = id,
@@ -20,3 +22,7 @@ fun InventoryEntity.toModel() = Inventory(
     barcodeUrl = barcodeUrl,
     gifticon = gifticon.toModel()
 )
+
+fun Iterable<InventoryEntity>.toModels() = map(InventoryEntity::toModel)
+
+fun List<InventoryEntity>.toModels() = map(InventoryEntity::toModel)
