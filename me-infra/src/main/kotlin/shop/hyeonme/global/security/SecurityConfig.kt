b465081f9378d46db397e390b68480bf9f4995ab
노현주 <dev.hyeonju@gmail.com>
2024-07-25 17:21:55 +0900

    add :: 경로 추가

--- me-infra/src/main/kotlin/shop/hyeonme/global/security/SecurityConfig.kt
+++ me-infra/src/main/kotlin/shop/hyeonme/global/security/SecurityConfig.kt
@@ -62,6 +62,7 @@ class SecurityConfig(
             .mvcMatchers(HttpMethod.GET, "/gifticon").authenticated()
             .mvcMatchers(HttpMethod.GET, "/gifticon/{id}").authenticated()
             .mvcMatchers(HttpMethod.POST, "/gifticon/admin").hasRole(ADMIN)
+            .mvcMatchers(HttpMethod.POST, "/gifticon/admin/{id}").hasRole(ADMIN)
 
             .anyRequest().authenticated()
             .and()
