5b30c3f8c4b2029f70a8d6b37d15d85c4ad27c46
노현주 <dev.hyeonju@gmail.com>
2024-07-26 12:30:07 +0900

    fix :: bean 추가

--- me-api/src/main/kotlin/shop/hyeonme/domain/coupon/service/impl/CommandCouponServiceImpl.kt
+++ me-api/src/main/kotlin/shop/hyeonme/domain/coupon/service/impl/CommandCouponServiceImpl.kt
@@ -1,9 +1,11 @@
 package shop.hyeonme.domain.coupon.service.impl
 
+import shop.hyeonme.common.annotation.CommandService
 import shop.hyeonme.domain.coupon.model.Coupon
 import shop.hyeonme.domain.coupon.service.CommandCouponService
 import shop.hyeonme.domain.coupon.spi.CouponPort
 
+@CommandService
 class CommandCouponServiceImpl(
     private val couponPort: CouponPort
 ) : CommandCouponService {
