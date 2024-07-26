1ee535f9a3e5329a61558bfca18280a0adafe539
노현주 <dev.hyeonju@gmail.com>
2024-07-26 12:21:08 +0900

    add :: 포인트 사용하는 로직 추가

--- me-api/src/main/kotlin/shop/hyeonme/domain/point/service/CommandPointService.kt
+++ me-api/src/main/kotlin/shop/hyeonme/domain/point/service/CommandPointService.kt
@@ -1,7 +1,9 @@
 package shop.hyeonme.domain.point.service
 
 import shop.hyeonme.domain.point.model.SavedPoint
+import shop.hyeonme.domain.point.model.UsedPoint
 
 interface CommandPointService {
     fun savePoint(savedPoint: SavedPoint): SavedPoint
+    fun usePoint(usedPoint: UsedPoint): UsedPoint
 }
